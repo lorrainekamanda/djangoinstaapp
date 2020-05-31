46de9ca0b00726802f738f91b6f41a608c9e871f
lorrainekamanda <lorrainekamanda@gmail.com>
2020-05-31 13:18:44 +0300

    adding delete and save images function

--- blog/models.py
+++ blog/models.py
@@ -4,14 +4,21 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+        
 class Profile(models.Model):
 
-    profile_image = models.ImageField(upload_to = 'images/',blank = True)
-    bio = models.TextField(max_length =1060,null = True)
+    profile_image = models.ImageField(default = 'profile.jpg',upload_to = 'images/')
+    bio = models.TextField(max_length =160,null = True)
+    phone_number = models.IntegerField(null = True)
+    user = models.OneToOneField(User,on_delete = models.CASCADE,null = True)
 
+    @classmethod
+    def search_by_username(cls,search_term):
+        uses = cls.objects.filter(user__username__icontains=search_term)
+        return uses
 
     def __str__(self):
-        return self.bio
+        return self.user.username
 
 class Image(models.Model):
 
@@ -21,7 +28,16 @@ class Image(models.Model):
     likes = models.IntegerField(null = True)
     videofile= models.FileField(upload_to='videos/', null=True, verbose_name="")
     profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null = True)
+    username= models.ForeignKey(User,on_delete=models.CASCADE,null = True)
+    
+    
+    def save_images(self):
+        self.save()
+   
+    def delete_images(self):
+        self.delete()
 
+  
     def __str__(self):
         return self.image
 
