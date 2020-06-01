5a8b0e8315c839776daefbd7f568cd819c894979
lorrainekamanda <lorrainekamanda@gmail.com>
2020-06-02 02:03:59 +0300

    individual image link

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,25 @@
 from django.conf.urls import url
 from . import views
+from .views import ImageView,ImageDetail,CreateDetail,UpdateDetail,CreateComment,CommentView
 from django.conf import settings
 from django.conf.urls.static import static
-urlpatterns = [
-    url('^$', views.register, name = 'blog-register'),
-    url('^home$', views.home, name = 'blog-home'),
-    url('^about/$', views.about, name = 'blog-about')
+from django.contrib.auth import views as auth_views
+from .models import Image
+from django.urls import path
+urlpatterns = [ 
+    
+    path('', ImageView.as_view(), name = 'blog-home'),
+    path('image/<int:pk>/', ImageDetail.as_view(), name = 'image-detail'),
+    path('image/<int:pk>/comment', CreateComment.as_view(), name = 'comment-detail'),
+    path('image/<int:pk>/update', UpdateDetail.as_view(), name = 'image-update'),
+    path('image/new/', CreateDetail.as_view(), name = 'image-create'),
+    path('register/', views.register, name = 'blog-register'),
+    path('profile/', views.profile, name = 'blog-profile'),
+    path('login', auth_views.LoginView.as_view(template_name = 'blog/login.html'), name = 'blog-login'),
+    path('logout', auth_views.LogoutView.as_view(template_name = 'blog/logout.html'), name = 'blog-logout'),
+    path('search/', views.search_results, name='search_results'),
+    
+
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
