fd40a7dcf17fadba0ed86b1bbab4105c0b31e82a
lorrainekamanda <lorrainekamanda@gmail.com>
2020-06-02 02:04:42 +0300

    add comment model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
-
+from django.urls import reverse
+from datetime import datetime
 # Create your models here.
 
 
@@ -10,26 +11,40 @@ class Profile(models.Model):
     profile_image = models.ImageField(default = 'profile.jpg',upload_to = 'images/')
     bio = models.TextField(max_length =160,null = True)
     phone_number = models.IntegerField(null = True)
-    user = models.OneToOneField(User,on_delete = models.CASCADE,null = True)
+    user = models.OneToOneField(User,on_delete = models.CASCADE,blank = True,null = True)
 
-    @classmethod
-    def search_by_username(cls,search_term):
-        uses = cls.objects.filter(user__username__icontains=search_term)
-        return uses
+   
 
     def __str__(self):
         return self.user.username
 
 class Image(models.Model):
 
-    upload_image = models.ImageField(upload_to = 'images/',blank = True)
-    caption = models.CharField(max_length =160,null = True)
-    comments = models.TextField(max_length =1160,null = True)
-    likes = models.IntegerField(null = True)
-    videofile= models.FileField(upload_to='videos/', null=True, verbose_name="")
-    profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null = True)
-    username= models.ForeignKey(User,on_delete=models.CASCADE,null = True)
-    
+    image = models.ImageField(upload_to = 'images/',blank = True)
+    caption = models.CharField(max_length =160,null = True,blank = True)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null = True,blank = True)
+    username= models.ForeignKey(User,on_delete=models.CASCADE)
+
+    @classmethod
+    def get_comments(cls,image_id):
+
+       image = Image.objects.get(pk = image_id)
+       image_comments = image.comments.set_all()
+       return image_comments
+
+
+    def get_absolute_url(self):
+        return reverse ('image-detail', kwargs = {'pk':self.pk})
+   
+
+    @classmethod
+    def search_by_username(cls,search_term):
+        uses = cls.objects.filter(username__user__icontains=search_term)
+        return uses
+
+    def get_image_by_id(id):
+        image = Image.objects.get(id = image_id)
+        return image
     
     def save_images(self):
         self.save()
@@ -39,5 +54,17 @@ class Image(models.Model):
 
   
     def __str__(self):
-        return self.image
+        return self.username.username
 
+class Comments(models.Model):
+    comments = models.TextField(max_length =1160)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null = True,blank = True)
+    likes = models.IntegerField(null = True,blank = True)
+    username= models.ForeignKey(User,on_delete=models.CASCADE)
+    image = models.ForeignKey(Image,on_delete=models.CASCADE)
+    
+    def get_absolute_url(self):
+        return reverse ('blog-home')
+
+    def __str__(self):
+        return self.username.username
