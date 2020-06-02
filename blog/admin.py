d006c62e259d6d0f8c6df313c48c6110899d23cf
lorrainekamanda <lorrainekamanda@gmail.com>
2020-06-02 14:57:16 +0300

    add preference table to the admin block

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Image,Profile,Comments
+from .models import Image,Profile,Comments,Preference
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Image)
 admin.site.register(Comments)
+admin.site.register(Preference)
