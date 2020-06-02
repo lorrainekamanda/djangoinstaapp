d3e92518cd6b9de464a44a95d2b0474dbe1b96b6
lorrainekamanda <lorrainekamanda@gmail.com>
2020-06-02 14:55:33 +0300

    add Preference Model,like and dislike button

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime
+from django.utils import timezone
 # Create your models here.
 
 
@@ -11,8 +12,8 @@ class Profile(models.Model):
     profile_image = models.ImageField(default = 'profile.jpg',upload_to = 'images/')
     bio = models.TextField(max_length =160,null = True)
     phone_number = models.IntegerField(null = True)
-    user = models.OneToOneField(User,on_delete = models.CASCADE,blank = True,null = True)
-
+    user = models.OneToOneField(User,on_delete = models.CASCADE,null = True)
+    
    
 
     def __str__(self):
@@ -20,11 +21,16 @@ class Profile(models.Model):
 
 class Image(models.Model):
 
-    image = models.ImageField(upload_to = 'images/',blank = True)
-    caption = models.CharField(max_length =160,null = True,blank = True)
-    profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null = True,blank = True)
-    username= models.ForeignKey(User,on_delete=models.CASCADE)
-
+    image = models.ImageField(upload_to = 'images/')
+    caption = models.CharField(max_length =160,null = True)
+    likes = models.IntegerField(default = 0)
+    dislikes = models.IntegerField(default = 0)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null = True)
+    username= models.ForeignKey(User,on_delete=models.CASCADE,null = True)
+    date_posted = models.DateTimeField(default=timezone.now)
+    @property
+    def number_of_comments(self):
+        return Comments.objects.filter(image=self).count()
     @classmethod
     def get_comments(cls,image_id):
 
@@ -57,14 +63,28 @@ class Image(models.Model):
         return self.username.username
 
 class Comments(models.Model):
-    comments = models.TextField(max_length =1160)
-    profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null = True,blank = True)
-    likes = models.IntegerField(null = True,blank = True)
-    username= models.ForeignKey(User,on_delete=models.CASCADE)
-    image = models.ForeignKey(Image,on_delete=models.CASCADE)
+    comments = models.TextField(max_length =1160,null = True)
+    profile = models.ForeignKey(Profile,on_delete=models.CASCADE,null = True)
+    likes = models.IntegerField(default=0)
+    username= models.ForeignKey(User,on_delete=models.CASCADE,null = True)
+    image = models.ForeignKey(Image,on_delete=models.CASCADE,null = True)
+    date_posted = models.DateTimeField(default=timezone.now)
     
     def get_absolute_url(self):
         return reverse ('blog-home')
 
     def __str__(self):
         return self.username.username
+
+
+class Preference(models.Model):
+    user= models.ForeignKey(User, on_delete=models.CASCADE,null = True)
+    image= models.ForeignKey(Image, on_delete=models.CASCADE,null = True)
+    value= models.IntegerField()
+    date= models.DateTimeField(auto_now= True)
+
+    def __str__(self):
+        return str(self.user) + ':' + str(self.image) +':' + str(self.value)
+
+    class Meta:
+       unique_together = ("user", "image", "value")
